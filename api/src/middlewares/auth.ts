07638d8a63fef0a1e289814cf89b7f35f817069d
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-05-15 10:52:31 +0200

    fix the issue when the user is not logged in. Now it does not let to access profile

--- api/src/middlewares/auth.ts
+++ api/src/middlewares/auth.ts
@@ -1,14 +1,34 @@
+// import { Request, Response, NextFunction } from "express";
+
+// const isLoggedIn = (req: Request, res: Response, next: NextFunction): void => {
+//   try {
+//     console.log(req.headers.cookie);
+//     next();
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       res.send({ message: error.message });
+//     } else {
+//       res.send({ message: "An unknown error occurred" });
+//     }
+//   }
+// };
+
+// export { isLoggedIn };
 import { Request, Response, NextFunction } from "express";
 
 const isLoggedIn = (req: Request, res: Response, next: NextFunction): void => {
   try {
-    console.log(req.headers.cookie);
-    next();
+    if (req.headers.cookie) {
+      console.log(req.headers.cookie);
+      next();
+    } else {
+      res.status(400).json({ message: "please login" });
+    }
   } catch (error: unknown) {
     if (error instanceof Error) {
-      res.send({ message: error.message });
+      res.status(500).json({ message: error.message });
     } else {
-      res.send({ message: "An unknown error occurred" });
+      res.status(500).json({ message: "An unknown error occurred" });
     }
   }
 };
